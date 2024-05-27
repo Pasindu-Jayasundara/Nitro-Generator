10ac4ff7f57b3a15777a6c983da16c8d406ced6b
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2024-05-27 21:38:07 +0530

    up

--- src/gui/SL_and_SM_and_TC_and_PL_Report.java
+++ src/gui/SL_and_SM_and_TC_and_PL_Report.java
@@ -4,12 +4,14 @@ import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import model.MySQL;
+import model.TopCustomerBean;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -94,22 +96,21 @@ public class SL_and_SM_and_TC_and_PL_Report extends javax.swing.JPanel {
     private void generateStockMovementReport(String fromDate, String toDate) {
 
         try {
-            JasperCompileManager.compileReport("/Report/stock_level.jrxml");
+            JasperCompileManager.compileReport("/Report/stock_movement.jrxml");
         } catch (JRException ex) {
             System.out.println("model.ReportPanel.generateReport()");
         }
 
         String query = "SELECT * FROM `stock` ON `stock`.`id`=`invoice_item`.`stock_id` "
                 + "INNER JOIN `product` ON `product`.`id`=`stock`.`product_id` "
-                + "INNER JOIN `category` ON `category`.`id`=`product`.`category_id` "
-                + "INNER JOIN `brand` ON `brand`.`id`=`product`.`brand_id` "
                 + "INNER JOIN `grn_item` ON `stock`.`id`=`grn_item`.`stock_id` "
                 + "INNER JOIN `grn` ON `grn_item`.`grn_id`=`grn`.`id` "
-                + "INNER JOIN `supplier` ON `supplier`.`mobile`=`grn`.`supplier_mobile` "
+                + "INNER JOIN `dam_stock` ON `dam_stock`.`stock_id`=`stock`.`id` "
                 + "WHERE `stock`.`add_date` BETWEEN ? AND ? ORDER BY `stock`.`id` ASC";
 
         try {
             ResultSet rs = MySQL.executeSearch(query, fromDate, toDate);
+            ResultSet rs2 = rs;
 
             DefaultTableModel dtm = new DefaultTableModel();
             dtm.setRowCount(0);
@@ -120,11 +121,32 @@ public class SL_and_SM_and_TC_and_PL_Report extends javax.swing.JPanel {
 
                 v.add(rs.getString("stock.id"));
                 v.add(rs.getString("product.name"));
-                v.add(rs.getString("brand.brand"));
-                v.add(rs.getString("category.category"));
-                v.add(rs.getString("supplier.company"));
-                v.add(rs.getString("supplier.mobile"));
-                v.add(rs.getString("stock.qty"));
+
+                String stockId = "";
+                int receivedQty = 0;
+                boolean addReceivedQty = false;
+
+                while (rs2.next()) {
+
+                    if (!rs2.getString("stock.id").equals(stockId)) {
+
+                        v.add(String.valueOf(receivedQty));//received qty
+                        addReceivedQty = true;
+                        break;
+                    } else {
+                        receivedQty += rs2.getInt("grn_item.qty");
+                    }
+                }
+
+                if (!addReceivedQty) {
+                    v.add(rs.getString("grn_item.qty"));//received qty
+                    receivedQty = rs.getInt("grn_item.qty");
+                }
+
+                v.add(rs.getString("grn_item.buying_price"));//unit price
+                v.add(String.valueOf(receivedQty - rs.getInt("stock.qty")));//sold qty
+                v.add(rs.getString("stock.selling_price"));//sold unit price
+                v.add(rs.getString("dam_stock.qty"));//damage qty
 
                 dtm.addRow(v);
 
@@ -139,7 +161,7 @@ public class SL_and_SM_and_TC_and_PL_Report extends javax.swing.JPanel {
             JRTableModelDataSource tmd = new JRTableModelDataSource(dtm);
 
             try {
-                JasperPrint jasperPrint = JasperFillManager.fillReport("/Report/stock_level.jasper", parameters, tmd);
+                JasperPrint jasperPrint = JasperFillManager.fillReport("/Report/stock_movement.jasper", parameters, tmd);
                 JasperViewer.viewReport(jasperPrint, false);
             } catch (JRException ex) {
                 Logger.getLogger(ReportPanel.class.getName()).log(Level.SEVERE, null, ex);
@@ -151,11 +173,130 @@ public class SL_and_SM_and_TC_and_PL_Report extends javax.swing.JPanel {
 
     }
 
-    private void generateTopCustomersReport() {
+    private void generateTopCustomersReport(String fromDate, String toDate) {
+
+        try {
+            JasperCompileManager.compileReport("/Report/top_customers.jrxml");
+        } catch (JRException ex) {
+            System.out.println("model.ReportPanel.generateReport()");
+        }
+
+        String query = "SELECT * FROM `customer` INNER JOIN `invoice` ON `customer`.`mobile`=`invoice`.`customer_mobile` "
+                + "WHERE `invoice`.`date` BETWEEN ? AND ? ORDER BY `invoice`.`paid_amount` DESC";
+
+        HashMap<String, TopCustomerBean> beanHash = new HashMap<>();
+        int totalCustomers = 0;
+
+        try {
+            ResultSet rs = MySQL.executeSearch(query, fromDate, toDate);
+
+            while (rs.next()) {
+
+                String currentInvoiceId = rs.getString("invoice.id");
+
+                if (!beanHash.containsKey(currentInvoiceId)) {//new
+
+                    TopCustomerBean tcb = new TopCustomerBean();
+                    tcb.setInvoiceId(currentInvoiceId);
+                    tcb.setPayments(rs.getDouble("invoice.paid_amount"));
+                    tcb.setTransactions(1);
+                    tcb.setMobile(rs.getString("customer.mobile"));
+                    tcb.setName(rs.getString("customer.fname"));
+
+                    beanHash.put(currentInvoiceId, tcb);
+                    totalCustomers++;
+
+                } else { //already in
+                    TopCustomerBean getBean = beanHash.get(currentInvoiceId);
+                    getBean.setPayments(getBean.getPayments() + rs.getDouble("invoice.paid_amount"));
+                    getBean.setTransactions(getBean.getTransactions() + 1);
+                }
+
+            }
+        } catch (Exception e) {
+        }
+
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.setRowCount(0);
+
+        for (TopCustomerBean bean : beanHash.values()) {
+
+            Vector<String> v = new Vector<>();
+            v.add(bean.getMobile());
+            v.add(bean.getName());
+            v.add(bean.getMobile());
+            v.add(String.valueOf(bean.getPayments()));
+            v.add(String.valueOf(bean.getTransactions()));
+
+            dtm.addRow(v);
+
+        }
+
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("From", String.valueOf(fromDate));
+        parameters.put("To", String.valueOf(toDate));
+        parameters.put("NoRegisteredCustomers", String.valueOf(totalCustomers));
+
+        JRTableModelDataSource tmd = new JRTableModelDataSource(dtm);
+
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport("/Report/top_customers.jasper", parameters, tmd);
+            JasperViewer.viewReport(jasperPrint, false);
+        } catch (JRException ex) {
+            Logger.getLogger(ReportPanel.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
-    private void generateProfitAndLossReport() {
+    private void generateProfitAndLossReport(String fromDate, String toDate) {
+
+        try {
+            JasperCompileManager.compileReport("/Report/profit_and_loss_report.jrxml");
+        } catch (JRException ex) {
+            System.out.println("model.ReportPanel.generateReport()");
+        }
+
+        double totalRevenue = 0.0;
+        double totalCost = 0.0;
+        double profit = 0.0;
+
+        String revenue = "SELECT SUM(`invoice`.`paid_amount`) AS `totalRevenue` FROM `invoice` "
+                + "WHERE `invoice`.`date` BETWEEN ? AND ?";
+        try {
+            ResultSet revenueRs = MySQL.executeSearch(revenue, fromDate, toDate);
+            if (revenueRs.next()) {
+                totalRevenue = revenueRs.getDouble("totalRevenue");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(SL_and_SM_and_TC_and_PL_Report.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        String cost = "SELECT SUM(`grn`.`paid_amount`) AS `totalCost` FROM `invoice` "
+                + "WHERE `invoice`.`date` BETWEEN ? AND ?";
+        try {
+            ResultSet costRs = MySQL.executeSearch(cost, fromDate, toDate);
+            if (costRs.next()) {
+                totalCost = costRs.getDouble("totalCost");
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(SL_and_SM_and_TC_and_PL_Report.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        profit = totalRevenue - totalCost;
+        
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("From", String.valueOf(fromDate));
+        parameters.put("To", String.valueOf(toDate));
+        parameters.put("Total", String.valueOf(totalRevenue));
+        parameters.put("Cost", String.valueOf(totalCost));
+        parameters.put("Profit", String.valueOf(profit));
+        
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport("/Report/profit_and_loss_report.jasper", parameters);
+            JasperViewer.viewReport(jasperPrint, false);
+        } catch (JRException ex) {
+            Logger.getLogger(ReportPanel.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
     }
 
@@ -256,10 +397,10 @@ public class SL_and_SM_and_TC_and_PL_Report extends javax.swing.JPanel {
                     generateStockMovementReport(formattedFromDate, formattedToDate);
                     break;
                 case 6://top customer
-                    generateTopCustomersReport();
+                    generateTopCustomersReport(formattedFromDate, formattedToDate);
                     break;
                 case 7://finance
-                    generateProfitAndLossReport();
+                    generateProfitAndLossReport(formattedFromDate, formattedToDate);
                     break;
             }
 
